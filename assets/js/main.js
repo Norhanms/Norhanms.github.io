007e06aafc208ac88bf6293fd19893174b18566e
NorhanKhafaga <norhan.khafaga@gmail.com>
2018-08-28 11:41:39 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var mainnav=document.getElementById("mainnav");
+function open_menu(){
+    if(mainnav.style.display=="block"){
+        mainnav.style.display="none";
+    }else{
+        mainnav.style.display="block";
+        
+    }
+    
+
+}
